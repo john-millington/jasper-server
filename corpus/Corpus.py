8f27667463f0eb88b2ee48a28972b77673b850d0
John Millington <jmillington@sapient.com>
2018-04-20 12:09:30 +0100

    Some renaming

--- corpus/Corpus.py
+++ corpus/Corpus.py
@@ -22,19 +22,6 @@ class Corpus:
 
         return combined
 
-    def sentiment(self, resource):
-        sentiment = resource['sentiment']
-        if 'score' in resource:
-            score = resource['score']
-            if score < 5:
-                sentiment = 'negative'
-            elif score > 6:
-                sentiment = 'positive'
-            else:
-                sentiment = 'neutral'
-        
-        return sentiment
-
     def sample(self, size, type, resource, mutate = False):
         info = json.load(open('./corpus/data/{}/info.json'.format(resource)))
         max_chunks = int(min(info["chunks"][type], math.ceil(float(size) / info["chunk_size"])))
@@ -43,11 +30,7 @@ class Corpus:
         for x in range(1, max_chunks + 1):
             data = json.load(open('./corpus/data/{}/chunks/{}.chunk{}.json'.format(resource, type, x)))
             for line in data["lines"]:
-                if 'value' in line and 'sentiment' in line:
-                    sentiment = line['sentiment']
-                    if mutate == True:
-                        sentiment = self.sentiment(line)
-
-                    resources.append([self.features.get(line["value"]), sentiment])
+                if 'value' in line and 'classification' in line:
+                    resources.append([self.features.get(line["value"]), line['classification']])
 
         return resources[0:size]
