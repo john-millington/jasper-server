99165f317b58d93da6e782f53fa55d361d0e508a
John Millington <john_millington@hotmail.co.uk>
2018-04-24 15:58:22 +0100

    Multithreaded server

--- jasper-server.py
+++ jasper-server.py
@@ -1,20 +1,38 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
-from http.server import HTTPServer
-import time
+import sys, os, socket
+from socketserver import ThreadingMixIn
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 from server.JasperServer import JasperServer
 
-hostName = ''
-hostPort = 1000
+HOST = socket.gethostname()
 
-myServer = HTTPServer((hostName, hostPort), JasperServer)
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
 
+'''
+This sets the listening port, default port 8080
+'''
+if sys.argv[1:]:
+    PORT = int(sys.argv[1])
+else:
+    PORT = 1000
+
+'''
+This sets the working directory of the HTTPServer, defaults to directory where script is executed.
+'''
+if sys.argv[2:]:
+    os.chdir(sys.argv[2])
+    CWD = sys.argv[2]
+else:
+    CWD = os.getcwd()
+
+server = ThreadingSimpleServer(('0.0.0.0', PORT), JasperServer)
+print("Serving HTTP traffic from", CWD, "on", HOST, "using port", PORT)
 try:
-	myServer.serve_forever()
+    while 1:
+        sys.stdout.flush()
+        server.handle_request()
 except KeyboardInterrupt:
-	pass
-
-myServer.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+    print("\nShutting down server per users request.")
